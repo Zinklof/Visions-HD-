70ffc01e770e83a8a120c44d38da1b3b5b0f1f16
Zinklof <123103261+Zinklof@users.noreply.github.com>
2023-05-08 14:07:23 -0400

    minor
    addedfew things to house

---
+++ Assets/Code/mainscene/ComputerScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerScreen : MonoBehaviour
+{
+    public CanvasGroup canvas;
+
+    private bool computerHasBeenAproached;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!computerHasBeenAproached)
+        StartCoroutine(PutComputerOnScreen());
+    }
+
+    IEnumerator PutComputerOnScreen()
+    {
+        computerHasBeenAproached = true;
+        canvas.alpha = 1f;
+        yield return new WaitForSecondsRealtime(2.5f);
+        canvas.alpha = 0f;
+    }
+}
